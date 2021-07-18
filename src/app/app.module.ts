57a0500ddb61925cf8d77447cf72785ad583dcbd
alfredopaz <apazv@unsa.edu.pe>
2021-07-18 18:50:39 -0500

    Pasando datos al componente hobby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HobbyComponent } from './hobby/hobby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    HobbyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hobby/hobby.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hobby',
+  templateUrl: './hobby.component.html',
+  styleUrls: ['./hobby.component.css']
+})
+export class HobbyComponent implements OnInit {
+  @Input() nameHobby : any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
