1cc65affb946d4533406da08929cdcd897dfc6e3
alfredopaz <apazv@unsa.edu.pe>
2021-07-19 10:10:08 -0500

    Databinding con los datos de un formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { HobbyComponent } from './hobby/hobby.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HobbyComponent } from './hobby/hobby.component';
     HobbyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
