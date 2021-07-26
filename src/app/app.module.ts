c428b6e4291d3a234534efae391ba33bde106345
alfredopaz <apazv@unsa.edu.pe>
2021-07-26 11:18:05 -0500

    Ejemplo de rutas básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import { HobbyComponent } from './hobby/hobby.component';
 import { FormsModule } from '@angular/forms';
 
 import { DataService } from './data.service'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Route} from '@angular/router';
+
+const routes: Route[] = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'hello', component: HelloWorldComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    HobbyComponent
+    HobbyComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
